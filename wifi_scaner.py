e29dba86c9a02234ffcd513c768db27ee3600bd5
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-02-25 07:34:55 +0000

    scan wi-fi ap in my room

---
+++ wifi_scaner.py
@@ -0,0 +1,13 @@
+import network
+from time import sleep
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(True)
+
+while True:
+    ap = sta_if.scan()
+    for i in range(len(ap)):
+        print(str(ap[i][0])[2:-1])
+    print('___________')
+    print('*total*: ' + str(len(ap)))
+    print('___________')
+    sleep(3)
