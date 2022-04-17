40289d70a28c7843d4c1eef82a111d8f7bc3f284
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-04-17 11:24:31 +0300

    Объединены функции WI-FI в один модуль. Упрощает подключение к boot.py

---
+++ wifi_connecter.py
@@ -0,0 +1,25 @@
+import network
+
+
+def do_router_connect(login, password):
+    from time import sleep
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network:')
+        wlan.connect(login, password)
+        while not wlan.isconnected():
+            print('*', end='')
+            sleep(0.5)
+    print('\nyour router:', login)
+    print('network config:', wlan.ifconfig())
+    
+
+def do_ap_connect(ssid='MyEspAP', psw='11111111'):
+    ap = network.WLAN(network.AP_IF) # create access-point interface
+    ap.config(essid=ssid, password=psw) # set the ESSID of the access point
+    ap.config(max_clients=10) # set how many clients can connect to the network
+    ap.active(True)         # activate the interface
+    print('ssid: ' + ssid)
+    print('password: ' + psw)
+    print(ap.ifconfig())
