e15d0e7619ab5c7c2bec29910619a530a3e94fd7
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-04-22 11:19:27 +0300

    sens function added

--- temperature.py
+++ temperature.py
@@ -3,11 +3,25 @@ import onewire
 import time, ds18x20
 
 
-ow = onewire.OneWire(Pin(12))
-ds = ds18x20.DS18X20(ow)
-roms = ds.scan()
+def weather_sens(temp):
+    if temp < 17:
+        return 'It is Cold!'
+    elif temp > 21:
+        return 'It is Hot!'
+    else:
+        return 'Is is normally!'
 
-while True:
-    ds.convert_temp()
-    time.sleep(3)
-    print(ds.read_temp(roms[0]))
+def main():
+    ow = onewire.OneWire(Pin(12))
+    ds = ds18x20.DS18X20(ow)
+    roms = ds.scan()
+
+    while True:
+        ds.convert_temp()
+        time.sleep(3)
+        temp = ds.read_temp(roms[0])
+        print(weather_sens(temp))
+        
+        
+if __name__=="__main__":
+    main()
