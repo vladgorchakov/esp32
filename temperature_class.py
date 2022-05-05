3474fb8603a63079721867b9fb2cf29f0982c288
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-05-05 10:59:27 +0300

    created class Sensor for temp sensors

---
+++ temperature_class.py
@@ -0,0 +1,22 @@
+from machine import Pin
+import time, onewire, ds18x20
+
+class Sensor:
+    def __init__(self, pin, num):
+        self.__pin = pin
+        self.__num = num
+    
+    def connect_to_line(self):
+        ow = onewire.OneWire(Pin(self.__pin))
+        self.__ds = ds18x20.DS18X20(ow)
+        self.__roms = self.__ds.scan()
+    
+    def get_data(self):
+        self.__ds.convert_temp()
+        time.sleep(1)
+        temp = self.__ds.read_temp(self.__roms[self.__num])
+        return temp
+
+temp = Sensor(12, 0)
+temp.connect_to_line()
+print(temp.get_data())
