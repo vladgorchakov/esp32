462f80ba55aa4ef5495df4b24493fa8c86cbaa5b
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-05-22 19:36:52 +0200

    correct output

--- sensors/monitoring.py
+++ sensors/monitoring.py
@@ -28,23 +28,30 @@ def get_temp(ds, roms):
 
     return ds.read_temp(roms[0])
 
-def send_temp(temp):
+def send_to_th(sensor_data):
     url = 'https://api.thingspeak.com/update?api_key=S5C0MUHLNFR9FOEY&field1='
-    temp = str(temp)
-    #client = MQTTClient(client_id='7c:9e:bd:f4:c8:41', server='narodmon.ru', port=1883, user='vladislavzikstudy', password='k6tdd0pa')
-    #client.connect()    
-    #client.publish('dinartal/esp8266/temperature', temp)
+    temp = str(get_temp(*sensor_data))
+    print(temp)
     http_get(url + temp)
  
+def send_to_nm(sensor_data):
+    temp = str(get_temp(*sensor_data))
+    print(temp)
+    client = MQTTClient(client_id='7c:9e:bd:f4:c8:41', server='narodmon.ru', port=1883, user='vladislavzikstudy', password='k6tdd0pa')
+    client.connect()    
+    client.publish('dinartal/esp32/temperature', temp)
+    print('Data to nm')
+    
 def start():
-    while True:
-        temp = get_temp(*connect_to_sensor(26))
-        send_temp(temp)
-        print(temp)
-        time.sleep(15)        
+    sensor_data = connect_to_sensor(26)
+    timer = Timer(0)
+    timer.init(period=300000, mode=Timer.PERIODIC, callback=lambda t:send_to_th(sensor_data))
     
 def main():
-    pass
+    while True:
+        print('Hello')
         
 if __name__ == '__main__':
     start()
+    
+    
--- timers.py
+++ timers.py
@@ -1,4 +1,3 @@
 from machine import Timer
 timer = Timer(0)
-
-timer.init(period=2000, mode=Timer.PERIODIC, callback=lambda t:print('Hello'))
+timer.init(period=1000, mode=Timer.PERIODIC, callback=lambda t:print('Hello'))
