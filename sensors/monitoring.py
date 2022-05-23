0786ca4c2caade93add3ee7aba95e2846bb9e7c0
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-05-23 14:13:42 +0300

    'refactoring'

--- sensors/monitoring.py
+++ sensors/monitoring.py
@@ -18,6 +18,7 @@ def connect_to_sensor(pin):
     ow = onewire.OneWire(Pin(26))
     ds = ds18x20.DS18X20(ow)
     roms = ds.scan()    
+    
     return ds, roms
 
 
@@ -27,20 +28,22 @@ def get_temp(ds, roms):
 
     return ds.read_temp(roms[0])
 
+
 def send_to_th(sensor_data):
     url = 'https://api.thingspeak.com/update?api_key=S5C0MUHLNFR9FOEY&field1='
     temp = str(get_temp(*sensor_data))
     print(temp)
     http_get(url + temp)
  
+
 def send_to_nm(sensor_data):
     temp = str(get_temp(*sensor_data))
     print(temp)
     client = MQTTClient(client_id='7c:9e:bd:f4:c8:41', server='narodmon.ru', port=1883, user='vladislavzikstudy', password='k6tdd0pa')
     client.connect()    
     client.publish('dinartal/esp32/temperature', temp)
-    print('Data to nm')
     
+
 def start():
     sensor_data = connect_to_sensor(26)
     timer = Timer(0)
