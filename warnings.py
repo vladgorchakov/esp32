936dea61a94470dc1c1bbdf869b81b5dcc0dd363
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-05-27 17:26:05 +0300

    added output

--- warnings.py
+++ warnings.py
@@ -8,18 +8,21 @@ class LedIndicator:
         self.__blue_pin = machine.Pin(pin3)
     
     
-    def display_hadm_state(self, start_value, end_value, current_value) -> None:
+    def display(self, start_value, end_value, current_value) -> None:
         if current_value > end_value:
             self.__red_pin.on()
             self.__blue_pin.off()
             self.__green_pin.off()
+            print(f'HIGH')
             
         elif current_value < start_value:
             self.__blue_pin.on()
             self.__red_pin.off()
             self.__green_pin.off()
-        
+            print(f'LOW')
+            
         else:
             self.__green_pin.on()
             self.__blue_pin.off()
             self.__red_pin.off()
+            print(f'OK')
