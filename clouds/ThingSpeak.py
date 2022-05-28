adb8075bfbd791f251f813a4e5c3cbcf46ce64f6
Vladislav Gorchakov <vladislavzikstudy@gmail.com>
2022-05-28 15:08:40 +0200

    make method check_sending as @static

--- clouds/ThingSpeak.py
+++ clouds/ThingSpeak.py
@@ -8,40 +8,36 @@ class Channel:
         self.__channel_path = "update?api_key=" + api_key + '&' + 'field'
         print(self.__channel_path)
         self.__fields = {}
-        
-    
-    def add_field(self, id):
-        field_path = self.__channel_path + str(id) + '='
-        self.__fields[id] = Field(id, field_path, name='')
-        
-        
+
+    def add_field(self, field_id):
+        field_path = self.__channel_path + str(field_id) + '='
+        self.__fields[field_id] = Field(field_id, field_path, name='')
+
     @property
     def fields(self):
         return self.__fields
-    
-    
-    def write_field(self, data: float, id: int) -> None:
+
+    def write_field(self, data: float, field_id: int) -> None:
         addr = socket.getaddrinfo(self.__host, 80)[0][-1]
-        return self.__fields[id].write(self.__host, addr, data)
+        return self.__fields[field_id].write(self.__host, addr, data)
 
 
 class Field:
-    def __init__(self, id, path: str, name='') -> None:
-        self.__id = id
+    def __init__(self, field_id, path: str, name='') -> None:
+        self.__id = field_id
         self.name = name
         self.__value = 0
         self.__field_path = path
-   
-   
-    def check_sending(self, data) -> bool:
-        num_get = int(str(data, 'utf8').split('\n')[-1])
         
+    @staticmethod
+    def check_sending(data) -> bool:
+        num_get = int(str(data, 'utf8').split('\n')[-1])
+
         if num_get != 0:
             return num_get
         else:
             return False
-   
-    
+
     def write(self, host, addr, value) -> bool:
         path = self.__field_path + str(value)
         s = socket.socket()
@@ -49,17 +45,17 @@ class Field:
         s.send(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (path, host), 'utf8'))
         data = s.recv(1000)
         s.close()
-        
+
         if self.check_sending(data):
             self.__value = value
             return True
         else:
             return False
-        
+
     @property
     def value(self):
         return self.__value
-        
-        
+
+
 t = Channel('ZQT22H2MJ0E5QWYN')
 t.add_field(1)
